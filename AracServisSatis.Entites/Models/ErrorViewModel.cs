e67f9c062bb2f8f305203519c4cbfdfe1b3b8e3c
ÖZLEM <ZLEM@LAPTOP-9N0VFRPG>
2025-11-07 12:34:27 +0300

    güncellendi

--- AracServisSatis.Entites/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AracServisSatis.Entites.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
